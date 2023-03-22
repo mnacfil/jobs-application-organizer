28314f572e54e4a62af6d58de42950e40854576c
Melvin Nacfil <mnacfil@gmail.com>
2023-03-22 09:33:49 +0800

    add favIcon and change the title

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'normalize.css'
 import './index.css';
 
 const container = document.getElementById('root');
