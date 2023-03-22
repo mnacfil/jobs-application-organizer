7fa8735a795848b3f1482648d841941583d0fe59
Melvin Nacfil <mnacfil@gmail.com>
2023-03-22 10:17:03 +0800

    setup navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
+import {BiAlignLeft} from 'react-icons/bi'
+import {HiUserCircle} from 'react-icons/hi'
+import {AiFillCaretDown} from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <h2>Navbar</h2>
+    <Wrapper>
+      <div className="nav-center">
+        <div className="nav-toggle">
+          <BiAlignLeft />
+        </div>
+        <h2>Dashboard</h2>
+        <div className="nav-user">
+          <button>
+            <HiUserCircle />
+            <span>Melvin</span>
+            <AiFillCaretDown />
+          </button>
+        </div>
+      </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.nav`
+
+`;
 
 export default Navbar
