4f4f8e861a670e2ad1cc934f3ceb4c5906709043
mnacfil <mnacfil@gmail.com>
2023-03-26 16:20:15 +0800

    setup appLogo

--- src/app/components/AppLogo.js
+++ src/app/components/AppLogo.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const AppLogo = () => {
   return (
-    <div>Jobs organizer</div>
+    <Wrapper>
+      <h2>App Logo</h2>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.header`
+  padding: 1rem;
+  
+
+`;
 
 export default AppLogo
