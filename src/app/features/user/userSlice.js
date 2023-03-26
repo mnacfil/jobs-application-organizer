213f37c1b734f735abf01eb849d9ac10328965fb
mnacfil <mnacfil@gmail.com>
2023-03-26 21:27:11 +0800

    setup user feature

---
+++ src/app/features/user/userSlice.js
@@ -0,0 +1,20 @@
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
+
+const initialState = {
+    user: null,
+    isLoading: false,
+    isError: false
+}
+
+const registerUser = createAsyncThunk('user/register', async(thunkAPI) => {
+    console.log(thunkAPI);
+    console.log('register user');
+})
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+
+    }
+})
