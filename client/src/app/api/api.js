cc1fd2f581c81a3ddc339b778e2bbe1c471edf63
mnacfil <mnacfil@gmail.com>
2023-04-18 12:08:30 +0800

    use deploy url

--- client/src/app/api/api.js
+++ client/src/app/api/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import { getUserToken } from '../util/localStorage'
 
-const thirdPartyURL = 'https://jobify-prod.herokuapp.com/api/v1/toolkit';
-const localURL = 'http://localhost:5000/api/v1'
+const deployURL = 'https://mnacfil-jobs-organizer.onrender.com/api/v1';
+const localURL = 'http://localhost:5000/api/v1';
 
 export const customAxios = axios.create({
-    baseURL: localURL
+    baseURL: deployURL
 })
 
 customAxios.interceptors.request.use((config) => {
